04dbbca8c2cf74ae07462372e5c874bb3fc378bf
Samsonov Viktor Viktorovich <dezerqq26@gmail.com>
2025-05-11 15:11:02 +0300

    feat: added main page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import {FaLightbulb} from 'react-icons/fa'
-import SearchBar from '../SearchBar/SearchBar'
-import styles from './Header.module.scss'
-import QuickActions from "../QuickActions/QuickActions.tsx";
+import React from 'react';
+import { FaLightbulb } from 'react-icons/fa';
+import SearchBar from '../SearchBar/SearchBar';
+import QuickActions from '../QuickActions/QuickActions';
+import styles from './Header.module.scss';
 
 const Header: React.FC = () => (
     <header className={styles.header}>
-        <div className={styles.left}>
-            <FaLightbulb size={32}/>
-            <span className={styles.logoText}>LampShop</span>
-        </div>
-        <SearchBar/>
-        <div className={styles.icons}>
-            <QuickActions/>
+        <div className={styles.inner}>
+            <div className={styles.logo}>
+                <FaLightbulb />
+                <span>LampShop</span>
+            </div>
+            <div className={styles.search}><SearchBar /></div>
+            <div className={styles.actions}><QuickActions /></div>
         </div>
     </header>
-)
-
-export default Header
+);
+export default Header;
--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import styles from './NavMenu.module.scss'
-import { categories } from '../../mocks/products'
+import React, { useState } from 'react';
+import styles from './NavMenu.module.scss';
+import { categories } from '../../mocks/products';
 
-
-const NavMenu: React.FC = () => (
-    <nav className={styles.nav}>
-        <div className="container">
-            <ul>
-                {categories.map((cat, i) => (
-                    <li key={i} title={cat}>
-                        {cat}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    </nav>
-)
-
-export default NavMenu
+const NavMenu: React.FC = () => {
+    const [open, setOpen] = useState(false);
+    return (
+        <nav className={styles.nav}>
+            <div className={styles.header} onClick={() => setOpen(!open)}>
+                <span className={styles.title}>Категории</span>
+                <button className={styles.toggle} aria-expanded={open}>
+                    {open ? '−' : '+'}
+                </button>
+            </div>
+            <div className={`${styles.container} ${open ? styles.open : ''}`}>
+                <ul>
+                    {categories.map((c, i) => (
+                        <li key={i} title={c}>{c}</li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    );
+};
+export default NavMenu;
