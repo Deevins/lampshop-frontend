2a41ad68a4ca18a716c913e7474547336a903586
Samsonov Viktor Viktorovich <dezerqq26@gmail.com>
2025-05-11 14:30:38 +0300

    feat: added main page

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,23 @@ import NavMenu from './components/NavMenu/NavMenu'
 import Catalog from './components/Catalog/Catalog'
 import Footer from './components/Footer/Footer'
 
-
 const App: React.FC = () => (
-    <>
+    <div className="app">
         <Header />
         <NavMenu />
-        <div className="container">
-            <main>
-                <section style={{ paddingTop: '1rem', paddingBottom: '1rem' }}>
-                    <h1 >Магазин лампочек LampShop</h1>
-                    <p>
-                        Добро пожаловать в LampShop — ваш надежный партнер в мире освещения! У нас вы найдете лампочки всех типов: от классических и светодиодных до умных решений для «умного дома».
-                    </p>
-                    <p>
-                        Мы предлагаем качественную продукцию от ведущих производителей по выгодным ценам, с доставкой по всей России.
-                    </p>
-                </section>
-                <Catalog />
-            </main>
-        </div>
-        <Footer/>
-    </>
+        <main className="container main-content">
+            <section style={{ paddingTop: '1rem', paddingBottom: '1rem' }}>
+                <h1>Магазин лампочек LampShop</h1>
+                <p>
+                    Добро пожаловать в LampShop — ваш надежный партнер в мире освещения! У нас вы найдете лампочки всех типов: от классических и светодиодных до умных решений для «умного дома».
+                </p>
+                <p>
+                    Мы предлагаем качественную продукцию от ведущих производителей по выгодным ценам, с доставкой по всей России.
+                </p>
+            </section>
+            <Catalog />
+        </main>
+        <Footer />
+    </div>
 );
 export default App
